a6845feb97e1ae120d02863ea28adef81434a592
Myoung Ji Kim <66910643+myoungji-kim@users.noreply.github.com>
2021-12-23 00:17:11 +0900

    Update 수박수박수박수박수박수.py

--- Level_1/수박수박수박수박수박수.py
+++ Level_1/수박수박수박수박수박수.py
@@ -1,3 +1,4 @@
+# 풀이 1
 def solution(n):
     answer = '수'
     for i in range(2, n + 1):
@@ -5,4 +6,11 @@ def solution(n):
             answer += '박'
         else:
             answer += '수'
+    return answer
+
+# 풀이 2
+def solution(n):
+    answer = '수'
+    for i in range(2, n + 1):
+        answer = answer + '박' if i % 2 == 0 else answer + '수'
     return answer
