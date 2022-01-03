a985494aa4114f0f2e3ca4c90a4395054be7bdeb
Myoung Ji Kim <66910643+myoungji-kim@users.noreply.github.com>
2022-01-03 18:29:12 +0900

    Update 약수의 합.py

--- Level_1/약수의 합.py
+++ Level_1/약수의 합.py
@@ -1,3 +1,5 @@
+# 1
+# 시간 복잡도 1/2 * n
 def solution(n):
     answer = []
     for i in range(1, n//2 + 1): # n + 1까지 돌 필요가 없음
@@ -5,3 +7,15 @@ def solution(n):
             answer.append(i)
     answer = sum(set(answer))
     return answer + n
+
+# 2
+# 시간 복잡도 n ** 0.5
+def solution(n):
+    answer = 0
+    for i in range(1,int(n**0.5)+1):
+        if n%i == 0:
+            if i != n//i:
+                answer += (i+n//i)
+            else:
+                answer += i
+    return answer
