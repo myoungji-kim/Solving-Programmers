d56c4289b91082dc90c3fb4340986d86f555b399
Myoung Ji Kim <66910643+myoungji-kim@users.noreply.github.com>
2022-01-03 18:22:57 +0900

    Update 약수의 합.py

--- Level_1/약수의 합.py
+++ Level_1/약수의 합.py
@@ -1,7 +1,7 @@
 def solution(n):
     answer = []
-    for i in range(1, n + 1):
+    for i in range(1, n//2 + 1): # n + 1까지 돌 필요가 없음
         if n % i == 0:
             answer.append(i)
     answer = sum(set(answer))
-    return answer
+    return answer + n
